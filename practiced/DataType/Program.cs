8af34333282fe526050dcace78053d1e8d1daacb
timothy adigun <adigun.timothy@gmail.com>
2026-01-19 21:59:36 +0000

    practice string datatype in csharpe

---
+++ practiced/DataType/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter FirstName: ");
+        string firstName = Console.ReadLine();
+
+        Console.Write("Enter LastName: ");
+        string lastName = Console.ReadLine();
+
+        Console.WriteLine($"My name is {lastName}, {firstName}.");
+        // using static function
+        string lang = getUserInput("Enter favourite language: ");
+        Console.WriteLine($"My favourite language is {lang}");
+    }
+
+    static string getUserInput(string msg) {
+        Console.Write(msg);
+        string input = Console.ReadLine();
+        return input;
+    }
+}
