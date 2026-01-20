27dc7d00f55c64f078e975ae50215aab501bb00c
timothy adigun <adigun.timothy@gmail.com>
2026-01-20 00:44:38 +0000

    practicing string interpolation

--- practiced/DataType/Program.cs
+++ practiced/DataType/Program.cs
@@ -14,6 +14,13 @@ class Program
         // using static function
         string lang = getUserInput("Enter favourite language: ");
         Console.WriteLine($"My favourite language is {lang}");
+        //
+        // display in hex
+        for(int i = 16; i <= 32; i++)
+        {
+            Console.Write("{0}", i);
+            Console.WriteLine($"- 0x{i:X}, 0b{i:b}");
+        }
     }
 
     static string getUserInput(string msg) {
