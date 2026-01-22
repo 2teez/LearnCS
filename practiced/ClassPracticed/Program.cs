d067085065f9dc890e5e860619783063f05306cf
timothy adigun <adigun.timothy@gmail.com>
2026-01-22 12:51:16 +0000

    practice class and OPP in cs

---
+++ practiced/ClassPracticed/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    public static void Main()
+    {
+        Employee employee = new() { FirstName = "Elixir" };
+        Console.WriteLine(employee);
+        employee.SetName("Java", "Gosling");
+        Console.WriteLine(employee);
+    }
+}
