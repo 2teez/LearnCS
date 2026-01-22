86d930847c37e3caee3d49cbf327619350a8fc3e
timothy adigun <adigun.timothy@gmail.com>
2026-01-22 15:22:22 +0000

    using reader from cs file

--- practiced/ClassPracticed/DataStorage.cs
+++ practiced/ClassPracticed/DataStorage.cs
@@ -12,10 +12,30 @@ class DataStorage
         writer.WriteLine(employee.Salary);
         //
         writer.Dispose();
+        Console.WriteLine("Employee Data Written.");
     }
 
-    public static void Load(string firstName, string? lastName)
+    public static Employee Load(string firstName, string lastName)
     {
-
+        // get file to load
+        var file = new FileStream(firstName + lastName + ".dat", FileMode.Open);
+        var reader = new StreamReader(file);
+        Employee employee = new() { FirstName = "" };
+        employee.FirstName = reader.ReadLine() ??
+               throw new InvalidOperationException("FirstName not provided.");
+        employee.LastName = reader.ReadLine() ??
+                      throw new InvalidOperationException("LastName not provided.");
+        var salary = reader.ReadLine() ??
+                      throw new InvalidOperationException("LastName not provided.");
+        if (decimal.TryParse(salary, out decimal newSalary))
+        {
+            employee.Salary = newSalary;
+        }
+        else
+        {
+            employee.Salary = 0.00M;
+        }
+        reader.Dispose();
+        return employee;
     }
 }
