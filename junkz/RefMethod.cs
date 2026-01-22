3c55c4af0cc176274ec2009f30a8353f7632bdcc
timothy adigun <adigun.timothy@gmail.com>
2026-01-22 00:18:21 +0000

    using reference and value parameter in cs

---
+++ junkz/RefMethod.cs
@@ -0,0 +1,20 @@
+using System;
+
+class RefMethod
+{
+    public static void Main()
+    {
+        string first = "GoodBye";
+        string second = "Welcome";
+        Console.WriteLine($"Before Swap: {first} <> {second}");
+        Swap(ref first, ref second);
+        Console.WriteLine($"After Swap: {first} <> {second}");
+    }
+
+    static void Swap(ref string first, ref string second)
+    {
+        var temp = first;
+        first = second;
+        second = temp;
+    }
+}
