1b0c03bad5851f31f03cc509476159e16fa2d7db
timothy adigun <adigun.timothy@gmail.com>
2026-01-24 21:44:54 +0000

    still practing class in cs

---
+++ junkz/PracticedClass/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+class PracticedClass
+{
+    public static void Main()
+    {
+        // using a default constructor in cs
+        PersonDefClass.Person p = new PersonDefClass.Person() { Age = 24 };
+        p.FirstName = "Java";
+        p.LastName = "Gosling";
+        Console.WriteLine(p);
+        //
+        PersonClassWithCons.Person p2 = new PersonClassWithCons.Person(firstName: "Perl", lastName: "Larry", age: 34);
+        Console.WriteLine(p2);
+    }
+}
+
+namespace PersonDefClass
+{
+    class Person
+    {
+        public string FirstName { get; set; } = "";
+        public string LastName { get; set; } = "";
+        public int? Age { get; set; } = 0;
+
+        public override string ToString() =>
+            $"Person: firstname = {FirstName}, lastname = {LastName}, age = {Age}.";
+    }
+}
+
+namespace PersonClassWithCons
+{
+    class Person(string firstName, string lastName, int age = 0)
+    {
+        // using c#12 merged the class and it seprate constructor
+        /*public Person(string firstName, string lastName, int age = 0)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            }*/
+        public string FirstName { get; set; } = firstName;
+        public string LastName { get; set; } = lastName;
+        public int Age { get; } = age;
+
+        public override string ToString() =>
+            $"Person: firstname = {FirstName}, lastname = {LastName}, age = {Age}.";
+    }
+}
