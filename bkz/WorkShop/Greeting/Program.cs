15490a17ff1b8d304e6490860a93685b6714d98b
timothy adigun <adigun.timothy@gmail.com>
2026-01-26 21:10:25 +0000

    console reading from cli in cs

---
+++ bkz/WorkShop/Greeting/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Greeting
+{
+    class Program
+    {
+        public static void Main()
+        {
+            var name = GetName("Enter your name: ");
+            Console.WriteLine($"{TitleCase(name)}, we would go a long way in C#!.");
+        }
+
+        static string GetName(string msg = "Enter: ")
+        {
+            string input;
+            do
+            {
+                Console.Write(msg);
+                input = Console.ReadLine()!.Trim();
+                if (string.IsNullOrWhiteSpace(input))
+                {
+                    Console.WriteLine("Can't use empty string.");
+                }
+            } while (string.IsNullOrWhiteSpace(input));
+            return input;
+        }
+
+        static string TitleCase(string word) => $"{word[0..1].ToUpper()}{word[1..].ToLower()}";
+
+    }
+}
