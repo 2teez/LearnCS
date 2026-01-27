6851cc4a8629a997cac10a4fa7d9b00e2ee9372a
timothy adigun <adigun.timothy@gmail.com>
2026-01-27 20:24:24 +0000

    added the result of another mean function

--- bkz/Programming_C#12/Averages/Averages/Program.cs
+++ bkz/Programming_C#12/Averages/Averages/Program.cs
@@ -2,11 +2,19 @@
 
 using static AverageCalculator;
 
-public class Program
+internal class Program
 {
     public static void Main(string[] args)
     {
         var nums = new string[] { "4.3", "2.15", "0.95" };
         Console.WriteLine(ArithmeticMean(nums));
+        try
+        {
+            Console.WriteLine(ArithmeticAverage(args));
+        }
+        catch (InvalidOperationException ioe)
+        {
+            Console.WriteLine("No args from the cli");
+        }
     }
 }
