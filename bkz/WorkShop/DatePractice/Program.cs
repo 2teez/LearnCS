256d5fd5099564bcb56e5fde1635bb2039ff1cff
timothy adigun <adigun.timothy@gmail.com>
2026-01-27 03:19:27 +0000

    practice DateTime in cs

---
+++ bkz/WorkShop/DatePractice/Program.cs
@@ -0,0 +1,41 @@
+﻿
+namespace MainClass
+{
+    using PracticeDateTime;
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Person person = new() { Name = "Java", BirthYear = 1990 };
+            Console.WriteLine(person);
+        }
+    }
+}
+
+namespace PracticeDateTime
+{
+    class Person
+    {
+        public required string Name
+        {
+            get; set;
+        }
+
+        public required int BirthYear { get; set; }
+
+        public int GetAge() => DateTime.UtcNow.Year - BirthYear;
+
+        public override bool Equals(object? obj)
+        {
+            var p = (Person)obj;
+            if (p != null)
+            {
+                return false;
+            }
+            return this.Name == p?.Name && this.BirthYear == p?.BirthYear;
+        }
+
+        public override int GetHashCode() => base.GetHashCode();
+        public override string ToString() => $"Person: Name: {Name}, Age: {GetAge()}";
+    }
+}
