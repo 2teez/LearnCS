b132c47697cac44a6d718ef42552fcac244e7228
timothy adigun <adigun.timothy@gmail.com>
2026-01-27 13:15:22 +0000

    rewrote the main entry function

--- bkz/Programming_C#12/Averages/Averages/Program.cs
+++ bkz/Programming_C#12/Averages/Averages/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Averages;
+
+class Program
+{
+    internal static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
