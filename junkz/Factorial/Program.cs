a394a935915e5156d7837fb62c59db78c78e36fc
timothy adigun <adigun.timothy@gmail.com>
2026-01-30 08:10:53 +0000

    factorial project in cs

---
+++ junkz/Factorial/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Numerics;
+
+namespace Factorial
+{
+    class Program
+    {
+        public static void Main()
+        {
+            foreach (var number in 1..1000)
+            {
+                Console.WriteLine(Fac(45_000));
+            }
+        }
+
+        internal static BigInteger Fac(BigInteger number)
+        {
+            if (number <= 1)
+            {
+                return (BigInteger)1;
+            }
+            return number * Fac(number - 1);
+        }
+    }
+}
