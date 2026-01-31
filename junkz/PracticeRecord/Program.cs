2b4d11a256fb4290107116b1a72215c8ef337b67
timothy adigun <adigun.timothy@gmail.com>
2026-01-31 13:13:51 +0000

    show that the parameter can be used in different order

--- junkz/PracticeRecord/Program.cs
+++ junkz/PracticeRecord/Program.cs
@@ -1,6 +1,4 @@
-﻿
-using System;
-namespace PracticeRecord;
+﻿namespace PracticeRecord;
 
 class Program
 {
@@ -8,6 +6,8 @@ class Program
     {
         var java = new Person(Name: "Java", Age: 42);
         ShowPerson(java);
+        var elixir = new Person(Age: 12, Name: "Elixir");
+        ShowPerson(elixir);
     }
 
     internal static void ShowPerson(Person p)
