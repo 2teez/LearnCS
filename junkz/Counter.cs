f9097591a03d568e9537dd1b4fb479280d4bcaf8
timothy adigun <adigun.timothy@gmail.com>
2026-01-31 01:53:51 +0000

    practice class with private constructor

--- junkz/Counter.cs
+++ junkz/Counter.cs
@@ -7,10 +7,12 @@ class Counter
     {
         NumberGenerator counter = new();
         var counter2 = new NumberGenerator(5);
+        var counter3 = NumberGeneratorWithPrivateConstructor.GetInstance(10);
         for (var i = 0; i < 5; i++)
         {
             Console.WriteLine($"{counter}: {counter.GetNextValue}");
             Console.WriteLine($"{counter2}: {counter2.GetNextValue}");
+            Console.WriteLine($"{counter3}: {counter3.GetNextValue}");
         }
     }
 }
@@ -33,6 +35,27 @@ class NumberGenerator
         private set
         {
             _count = 0;
+        }
+    }
+}
+
+class NumberGeneratorWithPrivateConstructor
+{
+    private NumberGeneratorWithPrivateConstructor(int startNumber = default)
+    {
+        _count = startNumber;
+    }
+    internal static NumberGeneratorWithPrivateConstructor GetInstance(int number = default)
+    {
+        return new NumberGeneratorWithPrivateConstructor(number);
+    }
+    private int _count;
+    public int GetNextValue
+    {
+        get
+        {
+            _count += 1;
+            return _count;
         }
     }
 }
