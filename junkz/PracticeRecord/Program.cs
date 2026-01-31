19d861c55efd13fb61c1370c127c65597b17ca65
timothy adigun <adigun.timothy@gmail.com>
2026-01-31 02:35:46 +0000

    practicing record type as POD

---
+++ junkz/PracticeRecord/Program.cs
@@ -0,0 +1,19 @@
+﻿
+using System;
+namespace PracticeRecord;
+
+class Program
+{
+    public static void Main()
+    {
+        var java = new Person(Name: "Java", Age: 42);
+        ShowPerson(java);
+    }
+
+    internal static void ShowPerson(Person p)
+    {
+        Console.WriteLine(p);
+    }
+}
+
+public record Person(string Name, int Age);
