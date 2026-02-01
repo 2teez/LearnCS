aa1a38408cc0adca2435c6d6aadbc772b33dfcfa
timothy adigun <adigun.timothy@gmail.com>
2026-02-01 10:07:05 +0000

    using out to get info out of the method

--- junkz/InOutRef.cs
+++ junkz/InOutRef.cs
@@ -13,9 +13,18 @@ internal static class Arith
 {
     static (int, int) DivMod(int num, int deno)
     {
-        if (deno == 0) throw new ArugmentNullOperation($"Invalid Denominator: {deno}");
+        if (deno == 0)
+            throw new ArugmentNullOperation($"Invalid Denominator: {deno}");
         int remainder = num % deno;
         int value = num / deno;
         return (value, remainder);
+    }
+
+    static int DivMod(int num, int deno, out int remainder)
+    {
+        if (deno == 0)
+            throw new ArugmentNullOperation($"Invalid Denominator: {deno}");
+        remainder = num % deno;
+        return num / deno;
     }
 }
