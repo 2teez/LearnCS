6dc5b12cc0bd589767cc2ca6a1b9bbe47a868660
timothy adigun <adigun.timothy@gmail.com>
2026-02-04 13:03:54 +0000

    virtual static in interface used

---
+++ junkz/PracInterface/Program.cs
@@ -0,0 +1,27 @@
+﻿class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine(LeftHanded.Side);
+        ShowHandedness<RightHanded>();
+        ShowHandedness<LeftHanded>();
+    }
+
+    public static void ShowHandedness<T>() where T : IHanded
+    {
+        Console.WriteLine(T.Side);
+    }
+}
+
+public interface IHanded
+{
+    static virtual string Side => "Right";
+}
+
+public class LeftHanded : IHanded
+{
+    public static string Side => "Left";
+}
+
+public class RightHanded : IHanded
+{ }
