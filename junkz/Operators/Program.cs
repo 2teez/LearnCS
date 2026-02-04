5fdb94ce058d47edd5e10671c3e601505c1dcaaa
timothy adigun <adigun.timothy@gmail.com>
2026-02-04 09:18:33 +0000

    using operator overload and deconstruct in cs

---
+++ junkz/Operators/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Operations
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var p = new Person("gosling", "jvm");
+            Console.WriteLine(p.GetFullName);
+            p += new[] { "clojure", "kotlin" };
+            Console.WriteLine(p.GetFullName);
+        }
+    }
+
+    class Person(string firstName, string lastName)
+    {
+        private string FirstName { get; set; } = firstName;
+        private string LastName { get; init; } = lastName;
+        public string GetFullName => $"{FirstName}, {LastName}";
+        // Wanted to use operator overload to change person
+        // fullname by adding other names the person instance
+        // may have
+        public static Person operator +(Person p, string[] names)
+        {
+            if (names is null || names.Length == 0)
+                return p;
+            var firstname = p.FirstName;
+            foreach (var name in names)
+            {
+                firstname += $" {name}";
+            }
+
+            return new Person(firstname, p.LastName);
+        }
+
+        public void Deconstruct(out string firstname, out string lastname)
+        {
+            firstname = FirstName;
+            lastname = LastName;
+        }
+    }
+}
