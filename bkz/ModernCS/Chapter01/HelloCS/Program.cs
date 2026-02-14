22fcbce5c142e7dd8a72d764d79a2f417582f740
timothy adigun <adigun.timothy@gmail.com>
2026-02-14 00:51:56 +0000

    using traditional main function in cs

---
+++ bkz/ModernCS/Chapter01/HelloCS/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace HelloCS;
+
+using static System.Console;
+using static System.Environment;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        WriteLine(
+            $"Namespace name is {typeof(Program).Namespace ?? "<null>"}");
+        WriteLine(CurrentDirectory);
+        WriteLine(Version);
+    }
+}
