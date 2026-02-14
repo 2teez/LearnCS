57d1edbb323d9d4f9cb281b6ad3d88af86cfddd4
timothy adigun <adigun.timothy@gmail.com>
2026-02-14 01:12:43 +0000

    about environment usage in cs

---
+++ bkz/ModernCS/Chapter01/AboutMyEnvironment/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace AboutMyEnvironment;
+
+using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(Environment.CurrentDirectory);
+        Console.WriteLine(Environment.OSVersion.VersionString);
+    }
+}
