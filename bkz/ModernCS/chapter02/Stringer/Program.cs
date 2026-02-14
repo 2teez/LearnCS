ea7e4769d7e7cf5263c2ff54e1b718e432f496dc
timothy adigun <adigun.timothy@gmail.com>
2026-02-14 10:47:29 +0000

    using verbatim string

---
+++ bkz/ModernCS/chapter02/Stringer/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Stringer;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        var person = new { FirstName = "Gosling", LastName = "Java", Age = 40 };
+        var javaJson = $$"""
+            {
+                "firstname": {{person.FirstName}},
+                "lastname" : {{person.LastName}},
+                "age": {{person.Age}},
+            }
+        """;
+
+        Console.WriteLine(javaJson);
+    }
+}
+
+readonly record struct Person(string FirstName, string LastName, int Age);
