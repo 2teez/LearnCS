f47a43adbc859160476a120fbd5cc6d9cfd5cfc8
timothy adigun <adigun.timothy@gmail.com>
2026-02-14 01:19:02 +0000

    all good

--- bkz/ModernCS/Chapter01/AboutMyEnvironment/Program.cs
+++ bkz/ModernCS/Chapter01/AboutMyEnvironment/Program.cs
@@ -8,5 +8,6 @@ class Program
     {
         Console.WriteLine(Environment.CurrentDirectory);
         Console.WriteLine(Environment.OSVersion.VersionString);
+        Console.WriteLine("Namespace: {0}", typeof(Program).Namespace ?? "<null>");
     }
 }
