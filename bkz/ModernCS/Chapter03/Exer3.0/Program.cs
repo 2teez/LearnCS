f82a2f17b20249a72eb8dc3649307505a4aa6d36
timothy adigun <adigun.timothy@gmail.com>
2026-02-14 18:04:14 +0000

    exercise practiced

---
+++ bkz/ModernCS/Chapter03/Exer3.0/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Exer3_0;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        for (int num = 0; num < 100; num++)
+        {
+            if (num % 10 == 0)
+            {
+                WriteLine();
+            }
+            RunFizzBuzz(num + 1);
+        }
+        WriteLine();
+    }
+
+    static void RunFizzBuzz(int number) => Write(number switch
+    {
+        var n when GetBoolValue(n, 15) => "FizzBuzz, ",
+        var n when n % 3 == 0 => "Fizz, ",
+        var n when n % 5 == 0 => "Buzz, ",
+        _ => $"{number}, ",
+    });
+
+    static bool GetBoolValue(int value, int divisor) => value % divisor == 0;
+}
