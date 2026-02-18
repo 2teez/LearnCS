90642d00936546e81a1a4a514d19b92641eb3aa2
timothy adigun <adigun.timothy@gmail.com>
2026-02-18 06:56:27 +0000

    testing return values using tuples

---
+++ junkz/ReturnValues/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace ReturnValues;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        Fruity myFruit = new() { Fruit = "Easy-Peeler" };
+        var (fruit, number) = myFruit.GetFruit();
+        Console.WriteLine($"{fruit}, {number}");
+        (fruit, number) = myFruit.GetFruit("apple", 6);
+        Console.WriteLine($"{fruit}, {number}");
+    }
+}
+
+class Fruity
+{
+    public required string Fruit;
+    public int number;
+    public (string, int) GetFruit(string fruit, int number)
+    {
+        Fruit = fruit;
+        this.number = number;
+        return (fruit, number);
+    }
+    public (string fruit, int number) GetFruit()
+    {
+        return (fruit: Fruit, number: this.number);
+    }
+}
