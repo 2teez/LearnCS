b9dee9146f57abd78131d2444d92dc9fcce957bc
timothy adigun <adigun.timothy@gmail.com>
2026-02-19 07:13:35 +0000

    added to the person.cs methods

--- bkz/ModernCS/Chapter06/PacktLibrary/Person.cs
+++ bkz/ModernCS/Chapter06/PacktLibrary/Person.cs
@@ -1,1 +1,22 @@
 ﻿namespace Packt.Shared;
+
+public class Person
+{
+    public string? Name { get; set; }
+    public DateTimeOffset Born { get; set; }
+    public List<Person> Children { get; set; } = new();
+    public List<Person> Spouses { get; set; } = new();
+
+    public void WriteToConsole()
+    {
+        WriteLine($"{Name} Born on {Born:dddd}");
+    }
+
+    public void WriteChildenToConsole()
+    {
+        string term = Children.Count == 1 ? "Child" : "Children";
+        WriteLine($"{Name} has  {Children.Count} {term}");
+    }
+
+    public bool IsMarried => Spouses.Count > 0;
+}
--- bkz/ModernCS/Chapter06/PeopleApp/Program.cs
+++ bkz/ModernCS/Chapter06/PeopleApp/Program.cs
@@ -4,6 +4,13 @@ public class Program
 {
     public static void Main(string[] args)
     {
-
+        Person harry = new()
+        {
+            Name = "Harry",
+            Born = new(year: 2001, month: 3, day: 25,
+        hour: 0, minute: 0, second: 0,
+        offset: TimeSpan.Zero)
+        };
+        harry.WriteToConsole();
     }
 }
